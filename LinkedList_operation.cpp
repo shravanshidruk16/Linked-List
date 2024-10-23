3d2e7f9ec810a7b2e7c03677ec4e4773cacd3f71
Shravan Shidruk <shravanshidruk1605@gmail.com>
2024-10-23 20:52:20 +0530

    Update LinkedList_operation.cpp

--- LinkedList_operation.cpp
+++ LinkedList_operation.cpp
@@ -1,6 +1,4 @@
-// In this program we will see how to push an element from front position and back position in the linked list
-// We will see how to pop an element from the front
-// Also how to insert an element at a given position
+
 #include <iostream>
 using namespace std;
 
@@ -25,7 +23,18 @@ public:
         head=NULL;
         tail=NULL;
     }
-    void push_front(int val){
+
+    void printLL(){ // to print the data elements from the list
+        Node* temp= head;
+        while(temp!=NULL){
+            cout<<temp->data<<"->";
+            temp=temp->next;
+        }
+        cout<<"NULL\n";
+    }
+
+
+    void push_front(int val){   // push the element from front
         Node* newNode = new Node(val);
 
         if(head==NULL){
@@ -38,7 +47,7 @@ public:
         }
     }
 
-    void push_back(int val){
+    void push_back(int val){  // push the element from back
         Node* newNode = new Node(val);
         if(head==NULL){
             head=tail=newNode;
@@ -50,16 +59,9 @@ public:
         }
     }
 
-    void printLL(){
-        Node* temp= head;
-        while(temp!=NULL){
-            cout<<temp->data<<"->";
-            temp=temp->next;
-        }
-        cout<<"NULL\n";
-    }
+    
 
-    void Insert(int val, int pos){
+    void Insert(int val, int pos){  // Insert data element at required position
         Node* newNode=new Node(val);
         Node* temp = head;
         for(int i=0 ;i<pos-1;i++){
@@ -74,7 +76,7 @@ public:
         temp->next=newNode;
     }
 
-    void pop_front(){
+    void pop_front(){ // remove the element from front 
         if(head==NULL){
             cout<<"LL is empty\n";
             return;
@@ -85,6 +87,74 @@ public:
         delete temp;
     }
 
+    int searchItr(int key){ // search the given key in LL and return the index
+        Node* temp=head;
+        int idx=0;
+        while(temp!=NULL){
+            if(temp->data==key){
+                return idx;
+            }
+            temp=temp->next;
+            idx++;
+            
+        }
+        return -1;
+    }
+
+    int helper(Node* h , int key){ // this func is used to search through recursion
+        if(h==NULL){
+            return -1;
+        }
+        if(h->data==key){
+            return 0;
+        }
+        int idx = helper(h->next,key);
+        if(idx==-1){
+            return -1;
+        }
+        return idx+1;
+    }
+    int searchRec(int key){  // the output of recursive function is stored here
+        return helper(head, key);
+    }
+
+    void reverseLL(){  // to reverse the LL
+        Node* curr=head;
+        Node* prev=NULL;
+        
+        while(curr!=NULL){
+            Node* next=curr->next;
+            curr->next=prev;
+            // now update curr and prev
+            prev=curr;
+            curr=next;
+        }
+        head=prev; /*from the above loop we get prev as last 
+        element so when we reverse the ll then the prev(last element) 
+        should be equal to head element of ll */
+    }
+
+    int Get_size(){  // to find the total data elements present in the linked list
+        int sz=0;
+        Node* temp = head;
+        while(temp!=NULL){
+            temp=temp->next;
+            sz++;
+        }
+        return sz;
+    }
+
+    void removeNth(int n){  // to remove Nth number element from backside of linked-list
+        int size= Get_size();
+        Node* prev=head;
+        for(int i=1 ; i<(size-n); i++){
+            prev=prev->next;
+        }
+        Node* toDel=prev->next;
+        cout<<"The element going to be deleted is:"<<toDel->data<<endl;
+        prev->next=prev->next->next;
+    }
+
 };
 
 int main(){
@@ -93,10 +163,19 @@ int main(){
     ll.push_front(2);
     ll.push_front(1);
     ll.push_back(4);
+    ll.push_back(5);
+    //ll.printLL();
+    //ll.Insert(100,2);
+    //ll.printLL();
+    //ll.pop_front();
+    //ll.printLL();
+    //cout<<"Through iterative search:"<<ll.searchItr(100)<<endl;
+    //cout<<"Through recursive search:"<<ll.searchRec(100)<<endl;
+    // cout<<"The reversed LL is: "<<endl;
+    //ll.reverseLL();
+    ll.printLL(); // 1->2->3->4->5
+    ll.removeNth(2);
     ll.printLL();
-    ll.Insert(100,2);
-    ll.printLL();
-    ll.pop_front();
-    ll.printLL();
+
     return 0;
 }
